fd24591846d6dba59cb18e33ef7e472c0abbf224
ktanaka117 <key.m4096.kung.fu@gmail.com>
2017-10-29 16:37:26 +0900

    [add] テストを追加

---
+++ test/requests/put_image_test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const assert = require('assert')
+const supertest = require('supertest')
+const server = require('../../server.js')
+
+const request = supertest(server.listen())
+
+describe('PUT /images', () => {
+	context('PUTで叩いたとき', () => {
+		it('bodyに文字列 "OK!" が返却されること', async () => {
+			const res = await request
+				.put('/images')
+
+			assert.equal(res.status, 200)
+			assert.equal(res.body, 'OK!')
+		})
+	})
+})
