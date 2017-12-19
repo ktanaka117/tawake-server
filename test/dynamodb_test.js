8942041f71425999ecc5b51c958bdf40e6d2ce0b
ktanaka117 <key.m4096.kung.fu@gmail.com>
2017-12-20 01:48:17 +0900

    コンテンツ取得のテストを実装

--- test/dynamodb_test.js
+++ test/dynamodb_test.js
@@ -7,23 +7,44 @@ const testtool = require('./dynamodb_testtool')
 
 const contentId = 'contentId'
 const contentUrl = 'contentUrl'
+const tags = ['白峰あやか', '黒沢ゆりね'].join('/')
 
 describe('DynamoDB', () => {
-	context('contentId, contentUrl, tagsが引数渡された場合', () => {
-		after(async () => {
-			await testtool.deleteContent(contentId)
+	context('addContent', () => {
+		context('contentId, contentUrl, tagsが引数渡された場合', () => {
+			after(async () => {
+				await testtool.deleteContent(contentId)
+			})
+	
+			it('引数に渡した内容が保存されること', async () => {
+				await dynamodb.addContent(contentId, contentUrl, tags)
+	
+				const content = await testtool.getContent(contentId)
+	
+				assert.equal(content.Item.content_id, contentId)
+				assert.equal(content.Item.content_url, contentUrl)
+				assert.equal(content.Item.tags, tags)
+			})
 		})
+	})
 
-		const tags = ['白峰あやか', '黒沢ゆりね'].join('/')
+	context('getContentByTag', () => {
+		context('tagを引数に渡した場合', () => {
+			before(async () => {
+				await dynamodb.addContent(contentId, contentUrl, tags)
+			})
 
-		it('引数に渡した内容が保存されること', async () => {
-			await dynamodb.addContent(contentId, contentUrl, tags)
+			after(async () => {
+				await testtool.deleteContent(contentId)
+			})
 
-			const content = await testtool.getContent(contentId)
+			it('tagを含む関連するコンテンツが取得できること', async () => {
+				const contents = await dynamodb.getContentByTag('白峰あやか')
 
-			assert.equal(content.Item.content_id, contentId)
-			assert.equal(content.Item.content_url, contentUrl)
-			assert.equal(content.Item.tags, tags)
+				assert.equal(contents[0].content_id, contentId)
+				assert.equal(contents[0].content_url, contentUrl)
+				assert.equal(contents[0].tags, tags)
+			})
 		})
 	})
 })
